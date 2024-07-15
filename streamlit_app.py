3e361929e3faec62d7d029cdc518f00cef2da79f
ms10841 <55449548+ms10841@users.noreply.github.com>
2024-07-15 11:28:29 -0400

    Update streamlit_app.py
    v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,29 @@
 import streamlit as st
 import pandas as pd
 
-# Required column headers based on PncImportDb.dbo.IMMUNIZATIONIMPORT
+# Required column headers
 required_columns = [
-    "RecordID", "PRIMARYKEY", "ACTION", "ID", "FIRSTNAME", "LASTNAME", "DOB",
-    "EVENTCODE", "EVENTNAME", "EVENTDATE", "ADDITIONALDATE", "BRANDNAME",
-    "MANUFACT", "LOTNUM", "ADMINBY", "RSLTCODE", "RSTLNUM", "LOC_CODE",
-    "LOC_DESC", "ACTIONDATE", "EDITDATE", "DELETEDATE"
+    "StartDate", "EndDate", "Status", "Progress", "Duration__in_seconds_", "Finished",
+    "RecordedDate", "DistributionChannel", "UserLanguage", "cantril1", "cantril2", 
+    "financial_cantril", "promis1", "promis2", "promis3", "K6_1", "K6_2", "K6_3", "K6_4", 
+    "K6_5", "K6_6", "mh_tx_v2", "loneliness1", "loneliness2", "loneliness3", "belonging_3", 
+    "diener1", "diener2", "diener3", "diener4", "diener5", "diener6", "diener7", "diener8", 
+    "expectancy1", "expectancy2", "binge", "campus1", "campus2", "campus3", "campus4", 
+    "campus5", "discrimination1", "discrimination2_2", "discrimination2_10", 
+    "discrimination2_1", "discrimination2_3", "discrimination2_4", "discrimination2_5", 
+    "discrimination2_6", "discrimination2_7", "discrimination2_8", "discrimination2_9", 
+    "discrimination2_9_TEXT", "discrimination3", "dem1", "dem2", "dem3", "dem4", "dem5_1", 
+    "dem6", "dem7", "dem8", "dem9", "dem10_1", "dem10_2", "dem10_3", "dem10_4", "dem10_5", 
+    "dem10_6", "dem10_7", "dem10_8", "dem10_9", "dem10_9_TEXT", "dem11", "Q145", "Q140", 
+    "Q147", "Q148", "promis1_recode", "promis2_recode", "promis3_recode", "k6_sum", 
+    "diener_sum", "cantril_categorical2", "loneliness_sum", "expectancy_value_mean_respondent", 
+    "expectancy_value_mean_total", "expectancy_value_stdev", "expectancy_value_cut", 
+    "expectancy_value_categorical", "promis_composite", "k6_categorical", "mh_tx_categorical", 
+    "cantril_categorical", "loneliness_categorical", "diener_categorical", 
+    "belonging_single_item_categorical", "binge_frequent", "binge_any", "health_academic_risk", 
+    "campus1_friend_categorical", "campus2_learning_categorical", "campus3_extracurricular_categorical", 
+    "campus4_mentor_categorical", "campus5_cares_categorical", "discrimination_any", "rdem_gender", 
+    "rdem_first_gen", "rdem_international", "rdem_degree"
 ]
 
 def validate_columns(df):
@@ -20,13 +37,9 @@ def validate_data_types(df):
     """ Validate data types of certain columns """
     # Define expected data types
     expected_types = {
-        "RecordID": "int64",
-        "DOB": "datetime64[ns]",
-        "EVENTDATE": "datetime64[ns]",
-        "ADDITIONALDATE": "datetime64[ns]",
-        "ACTIONDATE": "datetime64[ns]",
-        "EDITDATE": "datetime64[ns]",
-        "DELETEDATE": "datetime64[ns]"
+        "StartDate": "datetime64[ns]",
+        "EndDate": "datetime64[ns]",
+        "RecordedDate": "datetime64[ns]"
     }
     
     incorrect_types = {}
@@ -36,7 +49,8 @@ def validate_data_types(df):
     return incorrect_types
 
 def main():
-    st.title("CSV Upload and Validation")
+    st.title("Survey Validation Tool")
+    st.write("Welcome to the survey validation tool - this will review the file you upload and provide a message if it matches the correct format.")
     
     # File uploader
     uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
@@ -59,7 +73,10 @@ def main():
             else:
                 st.success("All data types are correct.")
                 
-            st.write("Data Preview:", df.head())
+            # Display 5 sample records where data is not NA
+            non_na_sample = df.dropna().head(5)
+            st.write("5 Sample Records where data is not NA:")
+            st.table(non_na_sample)
 
 if __name__ == "__main__":
     main()
